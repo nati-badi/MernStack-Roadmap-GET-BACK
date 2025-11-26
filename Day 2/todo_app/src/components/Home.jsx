2e99ddd7e0c20390b239da1be7f104689f33717c
Nati <natibadigojimma@gmail.com>
2025-11-26 12:38:35 +0300

    fixed minor bugs related to dependecies

--- Day 2/todo_app/src/components/Home.jsx
+++ Day 2/todo_app/src/components/Home.jsx
@@ -1,5 +1,11 @@
 function Home() {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-gray-200 p-6 rounded-lg">
+        <h1>To-Do App</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
